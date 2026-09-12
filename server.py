d28f7d108bc5a01765ac1deeb12c628b61cba215
ObserverZQ <657282348@qq.com>
2026-09-11 23:58:13 -0400

    feat(scoreboard): refresh scores after increment and sort descending

--- server.py
+++ server.py
@@ -51,6 +51,9 @@ def increase_score():
         if team["id"] == team_id:
             team["score"] += 1
 
+    # sort the whole scoreboard by scores in non-increasing order
+    scoreboard.sort(key = lambda team: team['score'], reverse = True)
+
     return jsonify(scoreboard=scoreboard)
 
 
--- static/scoreboard.js
+++ static/scoreboard.js
@@ -32,7 +32,8 @@ function increase_score(id){
     contentType: "application/json; charset=utf-8",
     data : JSON.stringify(team_id),
     success: function(result){
-        
+      // refresh the scoreboard immediately with the latest scoreboard data
+      display_scoreboard(result.scoreboard);
     },
     error: function(request, status, error){
         console.log("Error");
